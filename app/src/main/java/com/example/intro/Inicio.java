352788ff03a9ed647e4fd0482efa2a82284bc50a
HaolinZheng <hzheng@elpuig.xeill.net>
2024-02-25 15:13:18 +0100

    esta mal

--- app/src/main/java/com/example/intro/Inicio.java
+++ app/src/main/java/com/example/intro/Inicio.java
@@ -2,63 +2,91 @@ package com.example.intro;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Inicio#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.intro.databinding.FragmentInicioBinding;
+
 public class Inicio extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Inicio() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Inicio.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Inicio newInstance(String param1, String param2) {
-        Inicio fragment = new Inicio();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    TextView text1, text2, text3, text4;
+    Button button1, button2;
+    NavController navController;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false);
+        View view = inflater.inflate(R.layout.fragment_inicio, container, false);
+
+        text1 = view.findViewById(R.id.text1);
+        text2 = view.findViewById(R.id.text2);
+        text3 = view.findViewById(R.id.text3);
+        text4 = view.findViewById(R.id.text4);
+        button1 = view.findViewById(R.id.boton1);
+        button2 = view.findViewById(R.id.boton2);
+
+        escribirTexto(text1, "Bienvenido/a", 0);
+        return view;
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.iniciodesesion);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.registrarme);
+            }
+        });
+    }
+    private void escribirTexto(final TextView textView, final String textoCompleto, final int indice) {
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            if (indice <= textoCompleto.length()) {
+                textView.setText(textoCompleto.substring(0, indice));
+                escribirTexto(textView, textoCompleto, indice + 1);
+            } else {
+                switch (textoCompleto) {
+                    case "Bienvenido/a":
+                        escribirTexto(text2, "Doctor.........", 0);
+                        break;
+                    case "Doctor.........":
+                        escribirTexto(text3, "Espera,", 0);
+                        break;
+                    case "Espera,":
+                        escribirTexto(text4, "usted quien era?", 0);
+                        break;
+                    case "usted quien era?":
+                        button1.setVisibility(View.VISIBLE);
+                        button1.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.aparecion));
+                        button2.setVisibility(View.VISIBLE);
+                        button2.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.aparecion));
+                        break;
+                }
+            }
+        }, 50);
+    }
+
 }
--- app/src/main/java/com/example/intro/MainActivity.java
+++ app/src/main/java/com/example/intro/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.intro;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private VideoView videoView;
     private TextView textView;
     private ImageView imageView;
+    private Button button;
+
 
 
     @Override
@@ -23,9 +34,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        videoView = findViewById(R.id.video);
         imageView = findViewById(R.id.logo);
         textView = findViewById(R.id.texto);
+        button = findViewById(R.id.boton);
+        videoView = findViewById(R.id.video);
 
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.videoinicio;
         Uri uri = Uri.parse(videoPath);
@@ -35,13 +47,36 @@ public class MainActivity extends AppCompatActivity {
         videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
-                videoView.setVisibility(View.INVISIBLE);
+                videoView.setVisibility(View.GONE);
                 textView.setVisibility(View.VISIBLE);
                 imageView.setVisibility(View.VISIBLE);
+                button.setVisibility(View.VISIBLE);
                 textView.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.parpadeo));
             }
         });
 
         videoView.start();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirFragmento();
+                textView.setVisibility(View.GONE);
+                imageView.setVisibility(View.GONE);
+                button.setVisibility(View.GONE);
+            }
+        });
+    }
+    private void abrirFragmento() {
+        Inicio inicio = new Inicio();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        transaction.replace(android.R.id.content, inicio);
+
+        transaction.addToBackStack(null);
+
+        transaction.commit();
     }
 }
